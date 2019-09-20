d627dc6f723a3fcc6700fd68e43c662a4c0ce43d
Richa Gupta <richa.gupta@unthinkable.co>
2019-09-20 14:57:49 +0530

    DocString and comments added Total Number of Wrong Attempts Updated to 3. Bugs Fixed

--- guessTheWord.py
+++ guessTheWord.py
@@ -1,69 +1,106 @@
+'''-------------------Movie guess Game 
+	Developer : Richa Gupta
+	Created On : Sep 19, 2019
+	Modified On : Sep 20, 2019
+'''
 import random
 
 def edit_movie_name(movieName):
-	oMovieName = movieName
-	movieNameLen = len(movieName)
+	'''
+		This function edit movieName 
+		by replacing randomly selected indices 
+		with _. 
+	'''
+	
+	EditMovieName = movieName # creating a copy of movieName to edit  
+	movieNameLen = len(movieName) #Length of movie string
+
 	k = int(movieNameLen/3) # Getting Number of characters to be removed
 	indices = random.sample(list(range(0,movieNameLen)), k) # Generating k random indices
-	movieNameList = list(oMovieName)
 	
+	'''Replace characters at generated indices with _'''
 	for i in indices:
-		movieName = movieName[0:i] + '_' + movieName[i+1:] 
-	result = [movieName , indices, oMovieName]
+		EditMovieName = EditMovieName[0:i] + '_' + EditMovieName[i+1:]
+	 
+	result = [EditMovieName,indices,movieName] 
 	return result
 
-
 def choose_movie():
-	words = ['titanic','avatar','Gladiator','alien','vertigo','gandhi','rocky','jaws','Casablanca','inception','psycho','momento']
-	movie = random.choice(words)
-	return movie.lower()
+	'''
+		This function select a movie from available movies or movie list
+	'''
+	movies = ['titanic','avatar','Gladiator','alien','vertigo','gandhi','rocky','jaws','Casablanca','inception','psycho','momento']
+	movie = random.choice(movies) #To select a movie from available movie
+	return movie.lower() 
+
 
 def get_movie():
+	'''
+		This function select a movie from movie dataStore and 
+		return an edited movie name with original movie name and modified indexes
+	'''
 	movieName = choose_movie()
 	return edit_movie_name(movieName)
 
 def play() :
-	
-	while 1 == 1 :
-		start = input("\n\n\t\t\tDo You wanna Play Movie Guess Game? Press Y ")
+	''' 
+		Conducts the whole game 
+	''' 
+		
+	'''
+		Variables : 
+
+		editedMovieName : Movie Name with blanks. Keeps track of correctly guessed character till the time
+		blanks : list of blank indexes in movieName 
+		movieName : Original/ Full Movie Name
+		guessedMovieName : guessedMovieName keeps the track of movie guessed by user till the time
+		guesses : To keep track of wrong inputs/guess
+		
+	'''
+	while 1 == 1 : #Run the game until user quits
+		start = input("\n\n\t\t\tDo You wanna Play Movie Guess Game? Press Y ") #get input from user to start/continue the game
 		print('\n\n\n')
 		start = start.lower()
-		if  start != 'y' :
-			break;
-		selectedMovie = get_movie();
-		movieName = selectedMovie[0]
-		indices = selectedMovie[1]
-		oMovieName = selectedMovie[2]
-		print("\t\t\tYour Movie Name is : " ,movieName.upper())	
+		if  start != 'y' :  # Stop Game in case user choose not to contine
+			break
+		selectedMovie = get_movie() # Get a movie from movie dataStores
+		editedMovieName = selectedMovie[0]
+		blanks = selectedMovie[1]
+		blanks.sort()
+		movieName = selectedMovie[2] 
+		print("\t\t\tYour Movie Name is : ", editedMovieName.upper())	
 		
-		guessedMovieName = movieName
-		
-		indices.sort()
-		guesses = 3
-		for i in indices:
-			while guesses :
-				guessedChar = input("Enter your guess : ")
+		guessedMovieName = editedMovieName
+		guesses = 3 
+
+		for i in blanks :
+			while guesses > 0 :
+				guessedChar = input("Enter your guess : ") #guessed character entered by user
 				
-				print("\nYou Guessed : {} ".format(guessedMovieName[0:i] + guessedChar+'\u0332' +  guessedMovieName[i+1:]).upper())
-				guessedMovieName = guessedMovieName[0:i] + guessedChar +  guessedMovieName[i+1:]
+				print("\nYou Guessed : {} ".format(guessedMovieName[0:i] 
+								+ guessedChar+'\u0332' 
+								+  guessedMovieName[i+1:]).upper()) #Guessed Character will be underlined
 				
-				if oMovieName.startswith(guessedMovieName[0:i+1]) == False :
-					guesses = guesses-1 
-					if guesses > 0 :
+				guessedMovieName = guessedMovieName[0:i]+guessedChar+guessedMovieName[i+1:] # Create Guessed Movie Name String
+				
+				if guessedChar == '' or movieName.startswith(guessedMovieName[0:i+1]) == False : # Wrong Guess
+					guesses = guesses-1
+					if guesses > 0 : #In case or wrong guess check number of remaining attempts
 						print("\nWrong Guess, You left with only {} Guesses ".format(guesses))
-						print("\n \t\t\tYour Movie Name is : {} .".format(movieName.upper()))	
+						print("\n \t\t\tYour Movie Name is : {} .".format(editedMovieName.upper()))	
 						continue
 					else:
-						print("\n\n\n \t\t\tYou Loose ")
-				else:	
-					movieName = guessedMovieName
+						print("\n\n\n \t\t\t------Sorry! You Loose-------- ")
+				else:	#if guessed character is correct
+					editedMovieName = guessedMovieName
 					print("\nRight Guess. Go Ahead")
 					print("\n \t\t\tYour Movie Name is : {} .".format(guessedMovieName).upper())
 					break
 			else:
 				break
-		else :
-			print("\n\n\n \t\t\tYou win : Movie Name is {}".format(guessedMovieName.upper()))
+		else :	
+			if guessedMovieName == movieName :
+				print("\n\n\n \t\t\t--------Hurray!!! You win : Movie Name is {}-------".format(guessedMovieName.upper()))
 
 
 play()
